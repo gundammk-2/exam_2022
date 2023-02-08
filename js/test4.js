c10988f30e5fff8cef58ff264f5e06aa8abd745c
gandammk-2 <210008@yse.ac.jp>
2023-02-08 11:41:47 +0900

    first commit

---
+++ js/test4.js
@@ -0,0 +1,8 @@
+var priceElement = document.getElementById('price')
+console.log(priceElement)
+
+var price = priceElement.textContent
+console.log(price)
+
+var meesageElement = document.getElementById('message')
+meesageElement.innerHTML = "<span>いらっしゃい</span>"
